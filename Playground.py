7f94792139bda10a77a80a3c1a5c7235e0dc01cf
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-06-22 12:02:43 +1200

    Begun learning variables and printing

---
+++ Playground.py
@@ -0,0 +1,14 @@
+# Welcome to Python playground
+
+# We can print to console using print e.g
+
+print("Hello World!")
+
+# Printing two string together
+
+print("Hello " + "Flynn")
+
+# Creating variables
+
+todays_date = 22
+todays_day = "Wednesday"

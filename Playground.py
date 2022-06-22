5fd840686eaa7a3170534287f3cd0ea3a649a229
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-06-22 14:16:36 +1200

    Added integers and float values

--- Playground.py
+++ Playground.py
@@ -1,5 +1,7 @@
 # Welcome to Python playground
 
+# Comments on single lines are made with #
+
 # We can print to console using print e.g
 
 print("Hello World!")
@@ -24,5 +26,53 @@ remainder = 4 % 3
 print(multiply)
 print(remainder)
 
-#Updating Variables
+# Updating Variables
+# example using annual rainfall
+january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
+annual_rainfall = january_to_june_rainfall
 
+july_rainfall = 1.05
+annual_rainfall += july_rainfall
+
+august_rainfall = 4.91
+annual_rainfall += august_rainfall
+
+september_rainfall = 5.16
+annual_rainfall += september_rainfall
+
+october_rainfall = 7.20
+annual_rainfall += october_rainfall
+
+november_rainfall = 5.06
+annual_rainfall += november_rainfall
+
+december_rainfall = 4.06
+annual_rainfall += december_rainfall
+
+print(annual_rainfall)
+
+# Numbers
+
+# Integers are numbers with no decimal point E.G
+
+int1 = 1
+int2 = 2
+int3 = -3
+
+print(int1, ",", int2, ",", int3)
+
+# Float is a number with a decimal point
+# You can also use "e" when declaring to indicate the power of 10
+
+float1 = 1.0
+float2 = 10.0
+float3 = -5.5
+
+# Using "e"
+
+float4 = 1.5e2
+
+print(float1)
+print(float2)
+print(float3)
+print(float4)

4dfffbab130ec8095bf6aad1a465af3ddd5d93f8
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-06-23 14:52:01 +1200

    Added example code to build off of

--- converter.py
+++ converter.py
@@ -3,6 +3,7 @@ usd = input("Enter USD amount ")
 
 # Get user to confirm
 
+from unittest import result
 import click
 
 if click.confirm("You entered, " + str(usd) + " is this correct?", default=True):
@@ -12,35 +13,44 @@ if click.confirm("You entered, " + str(usd) + " is this correct?", default=True)
 
 print(
     str(
-        """CAD - C
-AUD - A
-NZD - N
-GBP - G
-CNY - Y
-EUR - E"""
+        """CAD - 1
+AUD - 2
+NZD - 3
+GBP - 4
+CNY - 5
+EUR - 6"""
     )
 )
-currency = input("Enter the desired currency (e.g for CAD, type C)")
+currency = input("Enter the desired currency (e.g for CAD, type )")
+
+# If statement for conversion
+
+if currency == 1:
+    print(str(usd * 1.2974511))
+     currency == 2:
+        print(str(usd * 1.4511509))
+
 
 # Math for conversions using match-case statement
 
-
+"""
 def convert(currency):
     match currency:
-        case "C", "c":
+        case 1:
             return usd * 1.2974511
-        case ":
+        case 2:
             return usd * 1.4511509
-        case N:
+        case 3:
             return usd * 1.5977626
-        case G:
+        case 4:
             return usd * 0.81711593
-        case Y:
+        case 5:
             return usd * 6.7117017
-        case E:
+        case 6:
             return usd * 0.94678294
         case _:
-            return "Error: Incorrect input"
+            return print("Error: Incorrect input")
 
 
-print(convert)
+print(convert(currency))
+"""

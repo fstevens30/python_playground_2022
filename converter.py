8408f914743c393db97551470af4b5669ad8ea38
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-07-18 14:25:07 +1200

    Fixed code to convert, changed usd variable to a float.

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
 # I aim to make a simple currency converter using an input of USD
-usd = input("Enter USD amount ")
+usd = float(input("Enter USD amount "))
 
 # Get user to confirm
 
@@ -21,16 +21,30 @@ CNY - 5
 EUR - 6"""
     )
 )
-currency = input("Enter the desired currency (e.g for CAD, type )")
+currency = input(str("Enter the desired currency (e.g for CAD, type 1)"))
 
-# If statement for conversion
-
-if currency == 1:
-    print(str(usd * 1.2974511))
-     currency == 2:
-        print(str(usd * 1.4511509))
+# Conversions
 
 
+def convert(currency):
+    match currency:
+        case "1":
+            return usd * 1.2974511
+        case "2":
+            return usd * 1.4511509
+        case "3":
+            return usd * 1.5977626
+        case "4":
+            return usd * 0.81711593
+        case "5":
+            return usd * 6.7117017
+        case "6":
+            return usd * 0.94678294
+        case _:
+            return print("Error: Incorrect input")
+
+
+print(convert(currency))
 # Math for conversions using match-case statement
 
 """

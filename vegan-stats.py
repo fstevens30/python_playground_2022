2f64b9538b90c866db3887120455770c449f4199
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-07-28 09:51:39 +1200

    Finsihed program

--- vegan-stats.py
+++ vegan-stats.py
@@ -37,21 +37,25 @@ import math
 
 #intro
 
-print("Welcome to the Vegan Statistics Calculator!\n")
+print("\nWelcome to the Vegan Statistics Calculator!\n")
 print("This tool will help you find out how much you can save over a period of time under a vegan lifestyle.\n")
 
 #initial math, converting day month and year to days
 
-numDays = int(input("How many days have you been vegan: \n"))
+numDays = int(input("\nHow many days have you been vegan: \n"))
 numMonths = int(input("How many months have you been vegan: \n"))
 numYears = int(input("How many years have you been vegan: \n"))
 
-totalDays = numDays + (numMonths * 30) + (numYears * 365)
+totalDays = numDays + (numMonths * 30.436875) + (numYears * 365.2425)
+
+#foramtting the days to be a whole number
+
+cleanDay = math.floor(totalDays)
 
 #display total time vegan
 
-print("You have been vegan for " + str(numYears) + " years, " + str(numMonths) + " months, and " + str(numDays) + " days.")
-print("That is a total of " + str(totalDays) + " days.\n")
+print("\nYou have been vegan for " + str(numYears) + " years, " + str(numMonths) + " months, and " + str(numDays) + " days.")
+print("That is a total of " + str(cleanDay) + " days.\n")
 
 #assigning variables
 
@@ -62,10 +66,17 @@ animalPerDay = 1
 
 #math
 
-grainSave = "{:.}".format(math.floor(grainPerDay * totalDays))
-litreSave = "{:.}".format(math.floor(litrePerDay * totalDays))
-forestSave = "{:.}".format(math.floor(forestPerDay * totalDays))
-animalSave = "{:.}".format(math.floor(animalPerDay * totalDays))
+grainSave = math.floor(grainPerDay * cleanDay)
+litreSave = math.floor(litrePerDay * cleanDay)
+forestSave = math.floor(forestPerDay * cleanDay)
+animalSave = math.floor(animalPerDay * cleanDay)
+
+#cleaning up savings with formatting
+
+grainSave = "{:,}".format(grainSave)
+litreSave = "{:,}".format(litreSave)
+forestSave = "{:,}".format(forestSave)
+animalSave = "{:,}".format(animalSave)
 
 #Print results
 
@@ -74,6 +85,4 @@ print("You have saved " + str(litreSave) + " litres of water.")
 print("You have saved " + str(forestSave) + " m2 of forest.")
 print("You have saved " + str(animalSave) + " animals.")
 
-
-
-
+exit()
